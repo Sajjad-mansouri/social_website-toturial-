25609c79285a1a94651848e8d58d38d279c83c05
Sajjad Mansouri <s.mansouri4w@gmail.com>
2023-04-24 16:48:56 +0330

    14- building follower syster

--- account/models.py
+++ account/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 from django.conf import settings
 
 class Profile(models.Model):
@@ -8,3 +9,20 @@ class Profile(models.Model):
 
 	def __str__(self):
 		return f'profile for {self.user}'
+
+
+class Contact(models.Model):
+	user_to=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='rel_to')
+	user_from=models.ForeignKey(settings.AUTH_USER_MODEL,on_delete=models.CASCADE,related_name='rel_from')
+	created=models.DateTimeField(auto_now_add=True)
+
+	class Meta:
+		ordering=['-created']
+	def __str__(self):
+		return f'{self.user_from} follow {self.user_to}'
+
+
+
+
+user_model=get_user_model()
+user_model.add_to_class('following',models.ManyToManyField('self',through=Contact,related_name='followers',symmetrical=False))
